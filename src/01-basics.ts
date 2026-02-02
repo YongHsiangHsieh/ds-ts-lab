a563185c2b1ea552873bffe52909475262d8a2df
Wilson <wilsonhsieh1216@gmail.com>
2026-02-02 12:15:09 +0000

    Added type-safety to colleague data

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -4,23 +4,34 @@ interface Friend {
   age: number;
 }
 
-const friend1 = {
+interface Colleague {
+  name: string;
+  department: string;
+  contact: {
+    email: string;
+    extension: number;
+  };
+}
+
+// (Optional) Change the names below to your friends.
+
+const friend1: Friend = {
   name: "Paul Fleming",
   phone: "087-12345",
   age: 25,
 };
 
-const friend2 = {
+const friend2: Friend = {
   name: "Jane Costello",
   phone: "086--12345",
   age: 31,
 };
 
-const friends = [friend1, friend2];
+const friends: Friend[] = [friend1, friend2];
 console.log(friends[1]);
 
 //   -------------------
-const colleague1 = {
+const colleague1: Colleague = {
   name: "Ralph Graham",
   department: "Engineering",
   contact: {
@@ -29,7 +40,7 @@ const colleague1 = {
   },
 };
 
-const colleague2 = {
+const colleague2: Colleague = {
   name: "Patti Burke",
   department: "Finance",
   contact: {
@@ -38,7 +49,7 @@ const colleague2 = {
   },
 };
 
-const colleague3 = {
+const colleague3: Colleague = {
   name: "Dean Sullivan",
   department: "HR",
   contact: {
@@ -46,6 +57,7 @@ const colleague3 = {
     extension: 125,
   },
 };
+
 const colleagues = {
   current: [colleague1, colleague2, colleague3],
   former: [],
