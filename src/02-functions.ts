067a29709b957ce1967141537861e9c5abca83f1
Wilson <wilsonhsieh1216@gmail.com>
2026-02-04 12:24:19 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -91,3 +91,14 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string): string[] {
+  if (friend.interests === undefined) {
+    friend.interests = [];
+  }
+  friend.interests.push(interest);
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], "Politics"));
+console.log(addInterest(friends[1], "Computing"));
